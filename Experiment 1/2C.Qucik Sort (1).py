2de95b9f92985f332cb20328f8d8e68d05c8753c
Nikithapitla <78146587+Nikithapitla@users.noreply.github.com>
2021-02-04 18:39:45 +0530

    Add files via upload

---
+++ Experiment 1/2C.Qucik Sort (1).py
@@ -0,0 +1,31 @@
+def quicksort(arr, start, end):
+  
+    if end - start > 1:
+        p = partition(arr, start, end)
+        quicksort(arr, start, p)
+        quicksort(arr, p + 1, end)
+ 
+ 
+def partition(arr, start, end):
+    pivot = arr[start]
+    i = start + 1
+    j = end - 1
+ 
+    while True:
+        while (i <= j and arr[i] <= pivot):
+            i = i + 1
+        while (i <= j and arr[j] >= pivot):
+            j = j - 1
+ 
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            arr[start], arr[j] = arr[j], arr[start]
+            return j
+ 
+ 
+arr = input('Enter the elements: ').split()
+arr = [int(x) for x in arr]
+quicksort(arr, 0, len(arr))
+print('Sorted list: ', end='')
+print(arr)
